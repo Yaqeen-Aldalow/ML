21b3721a7f957513d11b52a64e21bd45387ded55
borooj-edaily <109973333+borooj-edaily@users.noreply.github.com>
2024-12-29 14:09:25 +0200

    clean comment

--- CollaborativeFiltering.py
+++ CollaborativeFiltering.py
@@ -4,21 +4,16 @@ from surprise.model_selection import train_test_split
 from surprise import accuracy
 from surprise import KNNBasic
 
-# تحميل البيانات النظيفة
 ratings_df = pd.read_csv("clean_ratings.csv")
 
-# تحضير البيانات لاستخدامها مع surprise
 reader = Reader(rating_scale=(1, 5))
 data = Dataset.load_from_df(ratings_df[['user_id', 'movie_id', 'rating']], reader)
 
-# تقسيم البيانات إلى مجموعة تدريب واختبار (80% تدريب و 20% اختبار)
 trainset, testset = train_test_split(data, test_size=0.2)
 
-# تطبيق نموذج KNNBasic
 knn_model = KNNBasic()
 knn_model.fit(trainset)
 
-# تقييم النموذج
 knn_predictions = knn_model.test(testset)
 knn_rmse = accuracy.rmse(knn_predictions)
 knn_mae = accuracy.mae(knn_predictions)
